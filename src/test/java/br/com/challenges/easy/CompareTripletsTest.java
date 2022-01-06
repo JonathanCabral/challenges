cef500d828b63450bff655e4d1c124c9af1225de
Jonathan Pereira Cabral <dev.jonathancabral@gmail.com>
2022-01-06 19:01:24 -0300

    Correcao de pacotes

--- src/test/java/br/com/challenges/easy/CompareTripletsTest.java
+++ src/test/java/br/com/challenges/easy/CompareTripletsTest.java
@@ -1,6 +1,5 @@
 package br.com.challenges.easy;
 
-import easy.CompareTriplets;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
--- src/test/java/br/com/challenges/easy/SimpleArraySumTest.java
+++ src/test/java/br/com/challenges/easy/SimpleArraySumTest.java
@@ -1,6 +1,5 @@
 package br.com.challenges.easy;
 
-import easy.SimpleArraySum;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
