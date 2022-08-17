d8e037874c801a3d087fee1f1e6582acea020eb0
Jonathan Pereira Cabral <dev.jonathancabral@gmail.com>
2022-08-17 18:51:25 -0300

    Calculadora de numeros romanos

---
+++ src/main/java/br/com/challenges/easy/RomanNumbers.java
@@ -0,0 +1,67 @@
+package br.com.challenges.easy;
+
+/**
+ * @leetcode https://leetcode.com/problems/roman-to-integer/ (Roman numbers to int)
+ * @author jonathan
+ * @Date August, 16th 2022
+ */
+public class RomanNumbers {
+
+    public int romanToInt(String s) {
+        int sum = 0;
+
+        char letraAnterior = 'A';
+        for(int i = 0; i <= s.length()-1; i++) {
+            char letraAtual = s.charAt(i);
+            if(letraAtual == 'I') {
+                sum++;
+                letraAnterior = letraAtual;
+                continue;
+            }
+            if(letraAtual == 'V' ) {
+                if(letraAnterior == 'I')
+                    sum += 3;
+                else sum +=5;
+                letraAnterior = letraAtual;
+                continue;
+            }
+            if(letraAtual == 'X' ) {
+                if(letraAnterior == 'I')
+                    sum += 8;
+                else sum +=10;
+                letraAnterior = letraAtual;
+                continue;
+            }
+            if(letraAtual == 'L' ) {
+                if(letraAnterior == 'X')
+                    sum += 30;
+                else sum +=50;
+                letraAnterior = letraAtual;
+                continue;
+            }
+            if(letraAtual == 'C' ) {
+                if(letraAnterior == 'X')
+                    sum += 80;
+                else sum +=100;
+                letraAnterior = letraAtual;
+                continue;
+            }
+            if(letraAtual == 'D' ) {
+                if(letraAnterior == 'C')
+                    sum += 300;
+                else sum +=500;
+                letraAnterior = letraAtual;
+                continue;
+            }
+            if(letraAtual == 'M' ) {
+                if(letraAnterior == 'C')
+                    sum += 800;
+                else sum +=1000;
+                letraAnterior = letraAtual;
+                continue;
+            }
+        }
+
+        return sum;
+    }
+}
---
+++ src/test/java/br/com/challenges/easy/RomanNumbersTest.java
@@ -0,0 +1,97 @@
+package br.com.challenges.easy;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RomanNumbersTest {
+
+    private RomanNumbers roman;
+
+    @BeforeEach
+    public void setUp() {
+        this.roman = new RomanNumbers();
+    }
+
+    @Test
+    public void numeral_um_para_int() {
+        int value = roman.romanToInt("I");
+        assertEquals(1, value);
+    }
+
+    @Test
+    public void retornaValor2() {
+        int value = roman.romanToInt("II");
+        assertEquals(2, value);
+    }
+
+    @Test
+    public void retornaValor3() {
+        int value = roman.romanToInt("III");
+        assertEquals(3, value);
+    }
+
+
+    @Test
+    public void retornaValor5() {
+        int value = roman.romanToInt("V");
+        assertEquals(5, value);
+    }
+
+    @Test
+    public void retornaValor8() {
+        int value = roman.romanToInt("VIII");
+        assertEquals(8, value);
+    }
+
+    @Test
+    public void retornaValor50() {
+        int value = roman.romanToInt("L");
+        assertEquals(50, value);
+    }
+
+    @Test
+    public void retornaValor53() {
+        int value = roman.romanToInt("LIII");
+        assertEquals(53, value);
+    }
+
+    @Test
+    public void retornaValor55() {
+        int value = roman.romanToInt("LV");
+        assertEquals(55, value);
+    }
+
+    @Test
+    public void retornaValor100() {
+        int value = roman.romanToInt("C");
+        assertEquals(100, value);
+    }
+
+    @Test
+    public void retornaValor500() {
+        int value = roman.romanToInt("D");
+        assertEquals(500, value);
+    }
+
+    @Test
+    public void retornaValor1000() {
+        int value = roman.romanToInt("M");
+        assertEquals(1000, value);
+    }
+
+    // CORNER CASES
+
+    @Test
+    public void retornaValor4() {
+        int value = roman.romanToInt("IV");
+        assertEquals(4, value);
+    }
+
+    @Test
+    public void retornaValor9() {
+        int value = roman.romanToInt("IX");
+        assertEquals(9, value);
+    }
+}
